363a5e116c2db31799a33b1812c922837b974198
Alongedaniel <alongedaniel41@gmail.com>
2022-08-01 11:56:30 +0100

    Added fixes and media queries

--- Asset/js/script.js
+++ Asset/js/script.js
@@ -5,22 +5,22 @@ let allWeb = document.querySelectorAll(".web");
 let allApp = document.querySelectorAll(".app");
 
 webClick.addEventListener("click", function () {
-  allWeb.forEach((web) => (web.style.display = "block"));
+  allWeb.forEach((web) => (web.style.display = "inline-block"));
   allApp.forEach((app) => (app.style.display = "none"));
   webClick.style.color = "#758808";
   appClick.style.color = "#000";
 });
 
 appClick.addEventListener("click", function () {
-  allApp.forEach((app) => (app.style.display = "block"));
+  allApp.forEach((app) => (app.style.display = "inline-block"));
   allWeb.forEach((web) => (web.style.display = "none"));
   appClick.style.color = "#758808";
   webClick.style.color = "#000";
 });
 
 selectAll.addEventListener("click", function () {
-  allApp.forEach((app) => (app.style.display = "block"));
-  allWeb.forEach((web) => (web.style.display = "block"));
+  allApp.forEach((app) => (app.style.display = "inline-block"));
+  allWeb.forEach((web) => (web.style.display = "inline-block"));
   appClick.style.color = "#000";
   webClick.style.color = "#000";
 });
